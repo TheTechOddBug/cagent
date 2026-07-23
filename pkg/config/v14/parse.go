5b2da6b03715c7a4e1318643f1720e993d294409
David Gageot <david.gageot@docker.com>
2026-07-23 16:41:46 +0200

    freeze config v14 and start v15 as latest
    Assisted-By: docker-agent

--- pkg/config/v14/parse.go
+++ pkg/config/v14/parse.go
@@ -1,10 +1,10 @@
-package latest
+package v14
 
 import (
 	"github.com/goccy/go-yaml"
 
 	"github.com/docker/docker-agent/pkg/config/types"
-	previous "github.com/docker/docker-agent/pkg/config/v14"
+	previous "github.com/docker/docker-agent/pkg/config/v13"
 )
 
 func Register(parsers map[string]func([]byte) (any, error), upgraders *[]func(any, []byte) (any, error)) {
